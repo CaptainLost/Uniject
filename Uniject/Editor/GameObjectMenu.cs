7a219b597b50167edbd689a7233719e729f89bf2
CaptainLost <biggun632@gmail.com>
2025-01-14 09:46:06 +0100

    Added option to create project container from assets context menu

--- Uniject/Editor/GameObjectMenu.cs
+++ Uniject/Editor/GameObjectMenu.cs
@@ -1,3 +1,4 @@
+using System.IO;
 using UnityEditor;
 using UnityEngine;
 
@@ -27,6 +28,56 @@ namespace Uniject
             Undo.RegisterCreatedObjectUndo(sceneContext, "Create " + sceneContext.name);
 
             Selection.activeObject = sceneContext;
+        }
+    }
+
+    public static class AssetsMenu
+    {
+        [MenuItem("Assets/Create/Uniject/Project Container")]
+        static private void CreateProjectContainer(MenuCommand menuCommand)
+        {
+            string folderPath = "Assets";
+
+            Object selectedObject = Selection.activeObject;
+            if (selectedObject != null)
+            {
+                string selectedPath = AssetDatabase.GetAssetPath(selectedObject);
+
+                if (AssetDatabase.IsValidFolder(selectedPath))
+                {
+                    folderPath = selectedPath;
+                }
+                else
+                {
+                    folderPath = Path.GetDirectoryName(selectedPath);
+                }
+            }
+
+            string parentFolderName = Path.GetFileName(folderPath);
+
+            folderPath += "/Project Container.prefab";
+
+            if (parentFolderName != "Resources")
+            {
+                Logging.Warn("Project Container must be created in a 'Resources' folder.");
+
+                return;
+            }
+
+            GameObject projectContainer = new GameObject();
+            projectContainer.AddComponent<ProjectContainer>();
+
+            bool wasPrefabSuccess;
+
+            GameObject savedPrefab = PrefabUtility.SaveAsPrefabAsset(projectContainer, folderPath, out wasPrefabSuccess);
+            Selection.activeObject = savedPrefab;
+
+            GameObject.DestroyImmediate(projectContainer);
+
+            if (!wasPrefabSuccess)
+            {
+                Logging.Warn($"Failed creating Project Container in {folderPath}");
+            }
         }
     }
 }
